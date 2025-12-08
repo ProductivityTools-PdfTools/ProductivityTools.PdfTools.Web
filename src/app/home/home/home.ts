84400d51fd22cdd3bea476b2b6f62190a202740b
Pawel Wujczyk <pwujczyk@gmail.com>
2025-12-08 22:42:33 +0100

    force changes

--- src/app/home/home/home.ts
+++ src/app/home/home/home.ts
@@ -12,6 +12,10 @@ export class Home {
 
   name: string = "server did not respond"
 
+
+  file1 = signal<File | null>(null);
+  file2 = signal<File | null>(null);
+
   ngOnInit(): void {
     console.log('Home Component Initialized');
     this.homeService.sayHello("paweld").subscribe({
@@ -19,6 +23,29 @@ export class Home {
         console.log('GraphQL Response:', result);
         if (result.data && result.data.Hello) {
           this.name = result.data.Hello;
+          this.cdr.detectChanges();
+        }
+      },
+      error: (error: any) => {
+        console.error('GraphQL Error:', error);
+      }
+    });
+  }
+
+  onFile1Selected(event: any) {
+    this.file1 = event.target.files[0];
+  }
+
+  onFile2Selected(event: any) {
+    this.file2 = event.target.files[0];
+  }
+
+  uploadFiles() {
+    this.homeService.uploadFiles(this.file1, this.file2).subscribe({
+      next: (result: any) => {
+        console.log('GraphQL Response:', result);
+        if (result.data && result.data.UploadFiles) {
+          this.name = result.data.UploadFiles;
           this.cdr.detectChanges();
         }
       },
