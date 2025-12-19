44eccb35141a4bea6b833ae36471fcaecc406f56
Pawel Wujczyk <pwujczyk@gmail.com>
2025-12-19 21:04:44 +0100

    authentication

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB94zHv53jRwaHKApa6f_sZCrpe62yH8Ro",
+    authDomain: "ptauthumbrella.firebaseapp.com",
+    projectId: "ptauthumbrella",
+    storageBucket: "ptauthumbrella.firebasestorage.app",
+    messagingSenderId: "64395706392",
+    appId: "1:64395706392:web:10171948713a3baa78836d"
+};
